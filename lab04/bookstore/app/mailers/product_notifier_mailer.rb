e4fcd00eec4049c3bbe88e09334fb0745989b43b
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-06-20 15:32:56 +0300

    lab04

--- lab04/bookstore/app/mailers/product_notifier_mailer.rb
+++ lab04/bookstore/app/mailers/product_notifier_mailer.rb
@@ -2,7 +2,7 @@ class ProductNotifierMailer < ApplicationMailer
 
 
   def new_product_mail
-    mail(to: "ahmed@test.com", subject: "New product created")
+    mail(to: "mahmoud@test.com", subject: "New product created")
   end
 
 end
